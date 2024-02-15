cdd55ddc0406814cc0cfbdee2f8ef5162992b554
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-15 17:19:29 +0300

    getCurrentPosition and watchPosition

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 // GeoLocation API
+
+// ! Get Current Position
+function curSuccess(pos) {
+  const coords = pos.coords;
+  console.log(`Latitude: ${coords.latitude}`);
+  console.log(`Longitude: ${coords.longitude}`);
+  console.log(`Within: ${coords.accuracy} meters`);
+}
+
+function curError(error) {
+  console.log(`Error: ${error.code} - ${error.message}`);
+}
+
+const curOptions = {
+  enableHighAccuracy: true, // Use GPS if available
+  timeout: 5000, // Time to wait to stop trying for location
+  maximumAge: 0, // Do not use a cached position
+};
+
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+
+// ! WatchPosition
+
+const target = {
+  latitude: 41.7387463,
+  longitude: -71.2138621783,
+};
+
+function watchSuccess(pos) {
+  const coords = pos.coords;
+
+  if (
+    target.latitude === coords.latitude &&
+    target.longitude === coords.longitude
+  ) {
+    console.log("You have reached your destination!");
+    navigator.geolocation.clearWatch(id);
+  }
+}
+
+function watchError(error) {
+  console.log(`Error: ${error.code} - ${error.message}`);
+}
+
+const watchOptions = {
+  enableHighAccuracy: true, // Use GPS if available
+  timeout: 5000, // Time to wait to stop trying for location
+  maximumAge: 0, // Do not use a cached position
+};
+
+const id = navigator.geolocation.watchPosition(
+  watchSuccess,
+  watchError,
+  watchOptions
+);
